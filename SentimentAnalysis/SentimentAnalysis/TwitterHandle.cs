a189fff929a8a1d90c2570d9953140355a38eb0b
nishmittal <nishmittal91@gmail.com>
2015-04-30 20:19:52 +0100

    Added initial classes
    Initial classes to ensure repo is set up correctly.

---
+++ SentimentAnalysis/SentimentAnalysis/TwitterHandle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentimentAnalysis
+{
+    /// <summary>
+    /// Represents a Twitter Handle for which we store data.
+    /// </summary>
+    class TwitterHandle
+    {
+        public string Name { get; set; }
+        public int Followers { get; set; }
+        public int Retweets { get; set; }
+        public int Score { get; set; }
+
+        public TwitterHandle (string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
